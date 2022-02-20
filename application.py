4576fdfb850396d61d17bfa7ad4b7b7fbeaacda2
bmkuter <60325789+bmkuter@users.noreply.github.com>
2022-02-20 14:35:39 -0500

    Create application.py

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+# Documentation: https://flask.palletsprojects.com/en/2.0.x/quickstart/
+app = Flask(__name__)
+
+
+# Demo for Flask
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
